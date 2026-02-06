02648313b76fec4c9f2acf766faa7af23ee329aa
Koipix <koi.devt@gmail.com>
2026-02-06 21:21:10 +0800

    refc: modular compoments with better with api handling

---
+++ src/api/student/student_controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import * as studentService from "./student_serivce";
+
+//create student upon registration
+export async function studentRegistration(req: Request, res: Response) {
+    try {
+        const body = req.body;
+        if (!body.id) {
+            return res.status(400).json({
+                error: "Student Number is required!",
+            });
+        }
+
+        await studentService.createStudent(body);
+
+        return res.json({
+            status: "Success",
+        });
+    } catch (err) {
+        console.error(`Error: ${err}`);
+        res.status(500).json({
+            status: "Error",
+            message: "Server error nyae",
+        });
+    }
+}
+
+//fetch for unverified students
+export async function fetchUnverifiedStudents(req: Request, res: Response) {
+  const unverifiedStudentsList = await studentService.fetchUnverifiedStudents();
+  res.json(unverifiedStudentsList);
+};
