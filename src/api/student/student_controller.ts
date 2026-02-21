dc4d2c55d049a64eed3bed46c17d871910206e13
Koipix <koi.devt@gmail.com>
2026-02-22 03:32:28 +0800

    initial setup for fetching and submitting booking

--- src/api/student/student_controller.ts
+++ src/api/student/student_controller.ts
@@ -20,9 +20,46 @@ export async function studentRegistration(req: Request, res: Response) {
         });
     } catch (err) {
         console.error(`Error: ${err}`);
-        res.status(500).json({
-            status: "Error",
+        return res.status(500).json({
+            status: "Failed",
             message: "Server error nyae",
         });
     }
 }
+
+export async function fetchBooking(req: Request, res: Response) {
+    const list_booking = await studentService.fetchBooking();
+    return res.json(list_booking);
+}
+
+/* TODO:
+export async function createBooking(req: Request, res: Response) {
+    try {
+        const { student_number, boooking_id, period } = req.body;
+
+        if (!student_number || !boooking_id || !period) {
+            return res.status(400).json({
+                error: "Invalid Request!",
+            })
+        }
+
+        if (period !== 'morning' && period !== 'afternoon') {
+            return res.status(400).json({
+                error: "Invalid Request!",
+            })
+        }
+
+        await studentService.createBooking(student_number, boooking_id, period);
+
+        return res.json({
+            status: "Success"
+        });
+    } catch (err) {
+        console.error(`Error: ${err}`);
+        return res.status(500).json({
+            status: "Failed",
+            message: "Server error nyae",
+        });
+    }
+}
+*/
