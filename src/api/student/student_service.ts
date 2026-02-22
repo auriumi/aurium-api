8c92445f7c2ce8b05c8ca30326af22da19614f70
Koipix <koi.devt@gmail.com>
2026-02-22 20:06:12 +0800

    return student data upon request

--- src/api/student/student_service.ts
+++ src/api/student/student_service.ts
@@ -69,3 +69,40 @@ export async function createBooking(student_id: number, booking_id: number, peri
     console.error("Error: ", err);
   }
 }
+
+export async function getStudentProfile(student_number: number) {
+  try {
+    const student = await prisma.student.findUnique({
+      where: {
+        student_number: student_number
+      },
+      include: {
+        studentDetail: true,
+        StudentAuth: {
+          select: {
+            status: true,
+          },
+        },
+      },
+    });
+
+    if (!student) {
+      return {
+        success: false,
+        reason: "Student doesn't exist!"
+      };
+    }
+
+    return {
+      success: true,
+      student
+    };
+
+  } catch (err) {
+    console.error("Error: ", err);
+    return {
+      success: false,
+      reason: "Server error nyae"
+    };
+  }
+};
