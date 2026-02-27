2fa886e4338fcd39b72cca767ae1682ff9adfaf2
Koi <92094832+koipix@users.noreply.github.com>
2026-02-28 00:47:04 +0800

    feat: resend api integration to send temporary password (#29)
    * integrate resend api
    
    * refactor function to single responsiblity

--- src/api/admin/admin_service.ts
+++ src/api/admin/admin_service.ts
@@ -4,10 +4,12 @@ import bcrypt from "bcrypt";
 import { Resend } from 'resend';
 import { StudentStatus } from "@prisma/client";
 
-// const resend = new Resend(process.env.RESEND_API) needs a domain..
+const resend = new Resend(process.env.RESEND_API);
+const DOMAIN = "auriumi.cloud";
 
 export async function verifyStudent(id: string) {
   try {
+    //student lookup
     const student = await prisma.student.update({
       where: {
         student_number: parseInt(id),
@@ -23,14 +25,57 @@ export async function verifyStudent(id: string) {
         studentAuth: true,
       }
     });
+    if (!student) return { success: false, reason: "Student ID doesn't exist or already verified!" };
 
-    return student.studentAuth;
+    //generate temp pass and hash
+    const temp_pass = await generatePass();
+
+    //upload hashed pass
+    await prisma.student.update({
+      where : {
+        student_number: parseInt(id)
+      },
+      data: {
+        studentAuth: {
+          update: {
+            hashed_password: temp_pass.hash_pass,
+            status: StudentStatus.APPROVED,
+          },
+        },
+      },
+    });
+
+    //get the email
+    const get_email = await prisma.student.findUnique({
+      where: {
+        student_number: parseInt(id),
+      },
+      select: {
+        school_email: true,
+      }
+    }); 
+
+    if (!get_email) {
+      return { 
+        success: false,
+        reason: 'Student has no school email provided!'
+      };
+    }
+
+    //send credentials to the respective student email
+    const send_pass = await sendCreds(temp_pass.actual_pass, get_email.school_email);
+    if (!send_pass) return { success: false, reason: "Email API Error" };
+
+    return { success: true };
   } catch (err: any) {
-    return false;
+    return { 
+      success: false, 
+      reason: "Something went wrong!"
+    };
   }
 }
 
-export async function generatePass(id: string) {
+export async function generatePass() {
   const chars: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
   const charsLength: number = chars.length;
   const passLength = 10;
@@ -42,31 +87,28 @@ export async function generatePass(id: string) {
   }
   
   //hashing password with bcrypt
-  console.log("generated pass: ", tempPass);
+  console.log("generated pass: ", tempPass); //remove on prod
   const hashedPass = await bcrypt.hash(tempPass, 10);
-  
-  try {
-    const student = await prisma.student.update({
-      where : {
-        student_number: parseInt(id)
-      },
-      data: {
-        studentAuth: {
-          update: {
-            hashed_password: hashedPass,
-            status: StudentStatus.APPROVED,
-          },
-        },
-      },
-      include: {
-        studentAuth: true
-      }
-    });
 
-    return student.studentAuth;
-  } catch (err: any) {
-    console.error("err at password generation: ", err);
-  }
+  return {
+    actual_pass: tempPass,
+    hash_pass: hashedPass
+  };
+}
+
+//send temporary password to their email
+export async function sendCreds(pass: string, recipent: string) {
+  const { error } = await resend.emails.send({
+    from: `Aurium <noreply@${DOMAIN}>`,
+    to: recipent,
+    template: {
+      id: "password-verification",
+      variables: {
+        TEMP_PASSWORD: pass
+      },
+    },
+  });
+  return !error;
 }
 
 const STUDENTS_PER_PAGE = 8;
