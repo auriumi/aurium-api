6c1cf1e48ef65f2466b7a83bf6c17b19c1374afd
Koipix <koi.devt@gmail.com>
2026-03-31 08:33:23 +0800

    implementing rate limiter using the express limiter package

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { rateLimit } from "express-rate-limit";
+
 import studentRoutes from "./api/student/student_route"; 
 import adminRoutes from "./api/admin/admin_route";
 import authRoutes from "./api/auth/auth_route";
@@ -11,14 +13,29 @@ const corsConfig = {
   origin: 'http://localhost:3000',
   credentials: true,
 }
+
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use(cookieParser());
 
+const login_limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  limit: 5,
+  message: "Too many login attempts, please try again later",
+  legacyHeaders: false
+});
+
+const gen_limiter = rateLimit({
+  windowMs: 3 * 60 * 1000,
+  limit: 10,
+  message: "Too many request, please try again later :P",
+  legacyHeaders: false
+});
+
 //API ROUTES
-app.use("/api/student", studentRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/api/student", gen_limiter, studentRoutes);
+app.use("/api/admin", gen_limiter, adminRoutes);
+app.use("/api/auth", login_limiter, authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({
