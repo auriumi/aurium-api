12c1a642d64d0e5d965f4abfa329cafc9ac981ae
Koipix <koi.devt@gmail.com>
2026-08-03 22:56:09 +0800

    revert rate limits

--- src/server.ts
+++ src/server.ts
@@ -28,14 +28,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 const login_limiter = rateLimit({
-  windowMs: 3 * 60 * 1000, //5 mins (3mins ug)
+  windowMs: 5 * 60 * 1000, //5 mins
   limit: 10,
   message: "Too many login attempts, please try again later",
   legacyHeaders: false
 });
 
 const admin_limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, //3 mins (1mins ug)
+  windowMs: 3 * 60 * 1000, //3 mins
   limit: 100,
   message: "Too many request, please try again later :P",
   legacyHeaders: false
